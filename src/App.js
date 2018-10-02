5e5a41e3072c2d4a03af33d6ff68d45222d19259
shirin <s.khoramshahi@gmail.com>
2018-10-02 21:23:53 +0330

    fetch post

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import fetch from 'isomorphic-fetch';
+// import fetch from 'isomorphic-fetch';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.login = this.login.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      username: "",
+      password: '',
+      loginStatus: null
+    };
+  }
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
   login() {
-    console.log('test');
-    return fetch('http://api.symfony-3.dev/app_dev.php/posts', {
-      method: 'POST',
-      mode: 'CORS',
-      body: JSON.stringify({data:'test'}),
-      headers: {
-          'Content-Type': 'application/json'
+    console.log(this.state.username);
+    console.log(this.state.password);
+    const that = this;
+
+    fetch("https://panelt.mediaad.org/api/v2.0/advertiser/login", {
+      method: 'post',
+      body: JSON.stringify({action: "POST", 
+      data: {user_name: this.state.username, password: this.state.password}})
+    }).then(function(response) {
+      if(response.code === 200){
+        that.setState({loginStatus : <p>شما با موفقیت لاگین شدید:)</p>});
+      } else {
+        that.setState({loginStatus: <p>اطلاعات وارد شده اشتباه میباشد :(</p>});
+
       }
-  }).then(res => {
-      return res;
-  }).catch(err => console.log(err));
-    console.log('test1');
+      return ;
+    }).then(function(data) {
+      that.setState({loginStatus: <p>خطا در برقراری ارتباط با سرور :(</p>});
+      return ;
+    });
   }
   render() {
     return (
       <div className="App">
         <div className="container">
-          <h1> </h1>
+          <img src={'/hand.png'} alt="Welcome"/>
           <form className="form">
-            <input className="person" type="text" placeholder="Username" />
-            <input className="lock" type="password" placeholder="Password" />
-            <button onClick={this.login} type="submit" id="login-button">Login</button>
-
+            <input className="person" type="text" name="username" value={this.state.username} onChange={this.handleChange} placeholder="Username" />
+            <input className="lock" type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" />
+            <button onClick={this.login} type="button" id="login-button">Login</button>
 
           </form>
+         {this.state.loginStatus}
+        
           <p className="forgotPass">forgot password?</p>
 
           <button className="signUp" type="submit" id="login-button">Sign Up</button>
